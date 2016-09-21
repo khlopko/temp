2de48646e9eed4b77983d826480e2499d0bae573
Kirill Khlopko <k.khlopko@mobidev.biz>
2016-09-21 22:56:19 +0300

    Added new forms for views usage.

--- app/forms.py
+++ app/forms.py
@@ -4,5 +4,25 @@ from flask.ext.wtf import Form
 from wtforms import TextField, BooleanField
 from wtforms.validators import Required
 
+#
+# Login.
+#
+
 class LoginForm(Form):
     remember_me = BooleanField('remember_me', default = False)
+
+#
+# Add group.
+#
+
+class AddGroupForm(Form):
+	titleField = TextField('group_title', validators = [Required()])
+
+#
+# Add lector.
+#
+
+class AddLectorForm(Form):
+	firstname = TextField('firstname', validators = [Required()])
+	lastname = TextField('lastname', validators = [Required()])
+	sorname = TextField('sorname', validators = [Required()])
